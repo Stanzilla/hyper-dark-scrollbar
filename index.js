ba3347410e04c5e39867ab371722d373fa70bed8
Benjamin Staneck <Stanzilla@users.noreply.github.com>
2018-03-18 02:13:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 exports.decorateConfig = (config) => {
    return Object.assign({}, config, {
-        termCSS: `
-            ${config.termCSS || ''}
-            ::-webkit-scrollbar-thumb {
+        css: `
+            ${config.css || ''}
+            .xterm-viewport::-webkit-scrollbar-thumb {
                 background: #f1f1f0;
             }
             `
